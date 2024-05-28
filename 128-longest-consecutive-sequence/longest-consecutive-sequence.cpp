f20dcbc6591f07a874936624fec5feb6e00478cd
Vibhav Oswal <vibhavoswal3@gmail.com>
2024-05-28 15:13:26 +0530

    Time: 1497 ms (5.07%) | Memory: 135 MB (5.01%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -2,17 +2,20 @@ class Solution {
 private:
     unordered_map<int, int> m;
     unordered_map<int, int> len;
+    unordered_map<int, int> visited;
     //len will store the len starting from num
 
 public:
     int dfs(int u) {
+
+        visited[u] = 1;
+
         if(len[u] != 0) {
             return len[u];
         }
 
-        if(m[u+1] != 0) {
-            len[u+1] = 1 + dfs(u+1);
-            return len[u+1];
+        if(visited[u] != 0 and m[u+1] != 0) {
+            return 1 + dfs(u+1);
         }
 
         return 1;
@@ -27,8 +30,10 @@ public:
         int ans = 0;
 
         for(auto num: nums) { 
-            len[num] = dfs(num);
-            ans = max(ans, len[num]);
+            if(visited[num] == 0) {
+                len[num] = dfs(num);
+                ans = max(ans, len[num]);
+            }
         }
 
         return ans;
